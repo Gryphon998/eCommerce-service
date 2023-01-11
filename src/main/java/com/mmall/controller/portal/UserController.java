dd9c38050d1bdbc69a72ff2efd29597b1ace95a4
Grryyyyphon <cyosun1129@gmail.com>
2023-01-10 21:55:33 -0500

    Fix SpringMVC 404
    Now /user/login.do returns 500 instead of 404.
    Also added .gitignore

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -3,7 +3,6 @@ package com.mmall.controller.portal;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
-import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,111 +13,122 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
+/**
+ * Created by geely
+ */
 @Controller
-@RequestMapping
+@RequestMapping("/user/")
 public class UserController {
+
     @Autowired
     private IUserService iUserService;
 
-//    用户登录
-    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String  password, HttpSession session){
+    public ServerResponse<User> login(String username, String password, HttpSession session){
         ServerResponse<User> response = iUserService.login(username,password);
-            if (response.isSuccess()){
-                session.setAttribute(Const.CURRENT_USER,response.getData());
-            }
-
-            return  response;
+        if(response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
     }
 
-    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> logout(HttpSession session){
         session.removeAttribute(Const.CURRENT_USER);
         return ServerResponse.creatBySuccess();
     }
 
-    @RequestMapping(value = "register.do", method = RequestMethod.POST)
+    @RequestMapping(value = "register.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> register(User user){
         return iUserService.register(user);
     }
 
-    @RequestMapping(value = "check_valid.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> checkValid(String str, String type){
-        return iUserService.checkValid(str, type);
+    public ServerResponse<String> checkValid(String str,String type){
+        return iUserService.checkValid(str,type);
     }
 
-    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> getUserInfo(HttpSession session){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
-
-        if (user != null){
+        if(user != null){
             return ServerResponse.creatBySuccess(user);
         }
-
         return ServerResponse.creatByErrorMessage("Can not find user information");
     }
 
-    @RequestMapping(value = "fetch_security_question.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> fetchSecurityQuestion(String username){
+    public ServerResponse<String> forgetGetQuestion(String username){
         return iUserService.securityQuestion(username);
     }
 
-    @RequestMapping(value = "security_answer_check.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> securityAnswerCheck(String username, String question, String answer){
-        return iUserService.securityAnswerCheck(username, question, answer);
+    public ServerResponse<String> forgetCheckAnswer(String username,String question,String answer){
+        return iUserService.securityAnswerCheck(username,question,answer);
     }
 
-    @RequestMapping(value = "forget_reset_password.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "forget_reset_password.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> forgetResetPassword(String username, String newPassword, String forgetToken){
-        return iUserService.forgetResetPassword(username, newPassword, forgetToken);
+    public ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken){
+        return iUserService.forgetResetPassword(username,passwordNew,forgetToken);
     }
 
-    @RequestMapping(value = "reset_password.do", method = RequestMethod.POST)
+
+
+    @RequestMapping(value = "reset_password.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<String> resetPassword(HttpSession session, String password, String newPassword){
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (user == null){
+    public ServerResponse<String> resetPassword(HttpSession session,String passwordOld,String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
             return ServerResponse.creatByErrorMessage("Please login first");
         }
-        return iUserService.resetPassword(password, newPassword, user);
+        return iUserService.resetPassword(passwordOld,passwordNew,user);
     }
 
-    @RequestMapping(value = "update_info.do", method = RequestMethod.POST)
+
+    @RequestMapping(value = "update_information.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> updateInfo(HttpSession session, User user){
-        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser == null){
+    public ServerResponse<User> update_information(HttpSession session,User user){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
             return ServerResponse.creatByErrorMessage("Please login first");
         }
-
         user.setId(currentUser.getId());
         user.setUsername(currentUser.getUsername());
         ServerResponse<User> response = iUserService.updateInfo(user);
-
-        if (response.isSuccess()){
+        if(response.isSuccess()){
             response.getData().setUsername(currentUser.getUsername());
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
-
-        return  response;
+        return response;
     }
 
-    @RequestMapping(value = "show_user_info.do", method = RequestMethod.POST)
+    @RequestMapping(value = "get_information.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> showUserInfo(HttpSession session){
-        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if (currentUser == null){
-            return ServerResponse.creatByErrorMessageCode(ResponseCode.NEED_LOGIN.getCode(), "Please login first, status=10");
+    public ServerResponse<User> get_information(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.creatByErrorMessageCode(ResponseCode.NEED_LOGIN.getCode(),"Please login first, status=10");
         }
-
         return iUserService.showUserInfo(currentUser.getId());
     }
 }
